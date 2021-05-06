c869f4402cca62c6bc241d3a25ac5547077b5601
ShubhasisMathur <shubhasismathur@gmail.com>
2021-05-06 14:07:03 +0530

    Update provider.tf

--- CI CD Project/provider.tf
+++ CI CD Project/provider.tf
@@ -18,73 +18,9 @@ module "AKSCluster" {
 }
 
 
-provider "azuredevops" {
-  version = ">= 0.0.1"
-  personal_access_token = "${var.AZDO_PERSONAL_ACCESS_TOKEN}"
-  org_service_url = "${var.AZDO_ORG_SERVICE_URL}"
 
-}
 resource "azurerm_resource_group" "main" {
   name     = "${var.prefix}-resources"
   location = var.region
   //value="justforfun"
-}
-resource "azuredevops_project" "project" {
-  name       = "DemoforAKSInfra"
-  visibility         = "private"
-  version_control    = "Git"
-  work_item_template = "Agile"
-}
-resource "azuredevops_git_repository" "repository" {
-  project_id           = azuredevops_project.project.id
-  name                 = "microservices-reference-implementation"
-  initialization {
-    init_type = "Import"
-    source_type = "Git"
-    source_url = "https://github.com/ShubhasisMathur/microservices-reference-implementation.git"
-  }
-}
-
-resource "azuredevops_variable_group" "vars" {
-  project_id   = azuredevops_project.project.id
-  name         = "Infrastructure Pipeline Variables"
-  description  = "Managed by Terraform"
-  allow_access = true
-
-  variable {
-    name  = "FOO"
-    value = "BAR"
-  }
-}
-
-resource "azuredevops_build_definition" "build" {
-  project_id = azuredevops_project.project.id
-  name       = "Sample Build Definition"
-  path       = "\\ExampleFolder"
-
-  ci_trigger {
-    use_yaml = true
-  }
-
-  repository {
-    repo_type   = "TfsGit"
-    repo_id     = azuredevops_git_repository.repository.id
-    branch_name = azuredevops_git_repository.repository.default_branch
-    yml_path    = "azure-pipelines.yml"
-  }
-
-  variable_groups = [
-    azuredevops_variable_group.vars.id
-  ]
-
-  variable {
-    name  = "PipelineVariable"
-    value = "Go Microsoft!"
-  }
-
-  variable {
-    name      = "PipelineSecret"
-    secret_value     = "ZGV2cw"
-    is_secret = true
-  }
 }
